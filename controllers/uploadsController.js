9766847729a7c07c9c0fbc842da69c20ddc46def
oluwateezzy <oluwateezzy03@gmail.com>
2022-08-14 01:58:19 +0100

    controller

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,9 +1,28 @@
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
 const fs = require("fs");
+const path = require("path");
 
 const uploadProductImageLocal = async(req, res) => {
-    res.status(StatusCodes.OK);
+    if (!req.files) {
+        throw new CustomError.BadRequestError("No file uploaded");
+    }
+    const productImage = req.files.image;
+    if (!productImage.mimetype.startswith("image")) {
+        throw new CustomError.BadRequestError("Please Upload Image");
+    }
+    const maxSize = 1024 * 1024;
+    if (productImage.size > maxSize) {
+        throw new CustomError.BadRequestError("Please upload image smaller");
+    }
+    const imagePath = path.join(
+        __dirname,
+        "../public/uploads/" + `${productImage.name}`
+    );
+    await productImage.mv(imagePath);
+    res
+        .status(StatusCodes.OK)
+        .json({ image: { src: `/uploads/${productImage.name}` } });
 };
 
 const uploadProductImage = async(req, res) => {
